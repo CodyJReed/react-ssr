aad5da3d122d575bf8e445238bec23e59e72ab7d
Cody Reed <cody2j2reed@gmail.com>
2019-04-23 16:10:53 -0400

    Applied reducers to store(s)

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ import {createStore, applyMiddleware} from "redux"
 import thunk from "redux-thunk"
 import {Provider} from "react-redux"
 
+import reducers from "./reducers"
 import Routes from "./Routes"
 
 const store = createStore(reducers, {}, applyMiddleware(thunk))
--- src/helpers/createStore.js
+++ src/helpers/createStore.js
@@ -1,6 +1,8 @@
 import {createStore, applyMiddleware} from "redux"
 import thunk from "redux-thunk"
 
+import reducers from "../client/reducers"
+
 // Create a seperate store without a Provider
 // to prevent pre-rendering state errors
 // export for use within index.js route handling 
