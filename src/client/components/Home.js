e4f4052424ec460aea9649dc50aefb4c047831b9
Cody Reed <cody2j2reed@gmail.com>
2019-04-23 00:10:25 -0400

    Init Commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => (
+    <div>I'm a stateless Home Component</div>
+);
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const React = require("react");
+const renderToString = require("react-dom/server").renderToString;
+
+const Home = require("./client/components/Home").default
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) =>{
+    const content = renderToString( <Home />);
+    
+    res.send(content);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
