02fbe16add8bc48dcee04c2bc3aaf6efe776f532
Cody Reed <cody2j2reed@gmail.com>
2019-04-27 15:21:57 -0400

    Added AdminsListPage to /Routes

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage"
 import UsersListPage from "./pages/UsersListPage"
 import App from "./App"
 import NotFoundPage from "./pages/NotFoundPage"
+import AdminsListPage from "./pages/AdminsListPage"
 
 export default [
     {
@@ -15,12 +16,17 @@ export default [
                 exact: true
             },
             {
+                path: "/admins",
+                ...AdminsListPage
+            },
+            {
                 path: "/users",
                 ...UsersListPage
             },
             {
                 ...NotFoundPage
             }
+            
         ]
     }
 ]
